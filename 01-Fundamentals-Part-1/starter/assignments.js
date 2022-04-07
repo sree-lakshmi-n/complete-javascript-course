9a27c57690e50784cdefbb2de7876e0295f9e782
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-07 16:32:43 +0530

    Completed Values and Variables assignment: To log my country, continent, and population

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+// JavaScript Fundamentals – Part 1
+
+// LECTURE: Values and Variables
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+let country = 'India';
+let continent = 'Asia';
+let population = 1380000000;
+console.log("My country: " + country + "\nMy continent: " + continent +
+    "\nMy Population: " + population);
