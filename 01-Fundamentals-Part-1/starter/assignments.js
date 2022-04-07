a26e9ef7379b747ee4ded1a566d542dfa2a116ab
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-07 17:41:10 +0530

    Data types assignment : to use typeof operator

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,11 +1,24 @@
 // JavaScript Fundamentals – Part 1
 
 // LECTURE: Values and Variables
-/* 1. Declare variables called 'country', 'continent' and 'population' and
-assign their values according to your own country (population in millions)
-2. Log their values to the console */
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console 
 let country = 'India';
 let continent = 'Asia';
 let population = 1380000000;
 console.log("My country: " + country + "\nMy continent: " + continent +
     "\nMy Population: " + population);
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
