3613c0a18d671913997d1071a4eb7e27ce054dea
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-08 13:09:41 +0530

    Finds who has highest BMI using if-else statements and outputs the result using template literals.

--- 01-Fundamentals-Part-1/starter/codingchallenges.js
+++ 01-Fundamentals-Part-1/starter/codingchallenges.js
@@ -23,20 +23,33 @@ let markHeight = 1.69;
 let johnWeight = 92;
 let johnHeight = 1.95;
 
+// Data 2
+// markWeight = 95;
+// markHeight = 1.88;
+// johnWeight = 85;
+// johnHeight = 1.76;
+
 let markBMI = markWeight / markHeight ** 2;
 let johnBMI = johnWeight / johnHeight ** 2;
 let markHigherBMI = markBMI > johnBMI;
 console.log('Mark\'s BMI: ', markBMI, '\nJohn\'s BMI: ', johnBMI);
 console.log('Does Mark has higher BMI? ', markHigherBMI);
 
-// Data 2
-markWeight = 95;
-markHeight = 1.88;
-johnWeight = 85;
-johnHeight = 1.76;
-
 markBMI = markWeight / markHeight ** 2;
 johnBMI = johnWeight / johnHeight ** 2;
 markHigherBMI = markBMI > johnBMI;
 console.log('Mark\'s BMI: ', markBMI, '\nJohn\'s BMI: ', johnBMI);
 console.log('Does Mark has higher BMI? ', markHigherBMI);
+
+/*
+Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI.The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs.Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 😉
+*/
+if (markHigherBMI)
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+else
+    console.log(`John's BMI (${markBMI}) is higher than Mark's!(${johnBMI})`);
