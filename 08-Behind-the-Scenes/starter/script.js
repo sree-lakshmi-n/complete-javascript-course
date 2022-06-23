78e18c50f14f7cae47cca5d7678693c42ec7b899
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-23 13:13:22 +0530

    const block scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -5,6 +5,12 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName} is ${age}, born in ${birthYear}`;
     console.log(output);
+    // Block scope
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh! You're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
   }
   printAge();
   return age;
