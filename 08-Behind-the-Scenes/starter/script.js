015b4d0092f79ac5f79fd6685b3e0754f483a6b2
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-23 13:10:58 +0530

    Scoping practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  //   console.log(firstName);
+  function printAge() {
+    const output = `${firstName} is ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jo';
+calcAge(1991);
+// console.log(age);
+// printAge();
