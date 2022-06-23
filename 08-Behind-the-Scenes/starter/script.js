6b54fbe4ef02d7504eff4dada039b354a2ea0d45
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-23 13:28:05 +0530

    Creating new variable with same name as outer scope variable

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,6 +7,13 @@ function calcAge(birthYear) {
     console.log(output);
     // Block scope
     if (birthYear >= 1981 && birthYear <= 1996) {
+      /*
+        JS tries to look for the variable name in the current scope. Right now, firstName actually is in the current scope. So first name is indeed, in this same block. Therefore, JS will then use that variable and not perform any variable look up in the scope chain.
+        
+        So the scope chain isn't necessary at all, if the variable that we're looking for is already in the current scope.
+        */
+      // Creating new variable with same name as outer scope variable
+      const firstName = 'Steven';
       const str = `Oh! You're a millenial, ${firstName}`;
       var millenial = true;
       console.log(str);
@@ -16,6 +23,8 @@ function calcAge(birthYear) {
         return a + b;
       }
       add(2, 3);
+      // Creating new variable with same name as outer scope variable
+      const output = 'New Output!';
     }
     // var is function scoped
     console.log(millenial);
@@ -24,6 +33,7 @@ function calcAge(birthYear) {
 
     // functions are block scoped in strict mode
     // add(2, 3);
+    console.log(output);
   }
   printAge();
   return age;
