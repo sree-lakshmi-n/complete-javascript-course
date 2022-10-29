94d1b0492fed07ce97df674ec5bda1d7dca1c1ce
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-29 11:20:41 +0530

    measureKelvin()

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: prompt("Degree celsius: "),
+  };
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) Identify
+console.log(measureKelvin());
