79669bade8347fc1c768966b2a9b20244ea3fa02
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-12 08:00:49 +0530

    Coding Challenge #1 prompt

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,50 +53,6 @@ const restaurant = {
   },
 };
 
-////////////////////////////////////////////////////////////
-
-// Logical Assignment Operators
-
-const rest1 = {
-  name: 'Capri',
-  numGuests: 20,
-};
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
-
-// OR Assignment Operator
-
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
-console.log(rest1.numGuests, rest2.numGuests);
-// Works beautifully except when 0 is encountered.
-rest1.numGuests = 0;
-
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
-console.log(rest1.numGuests, rest2.numGuests);
-
-// Nullish Assignment Operator
-// Solves 0 issue
-rest1.numGuests = 0;
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-console.log(rest1.numGuests, rest2.numGuests);
-
-// AND Assignment Operator
-// Making restaurant owner's name anonymous
-rest1.owner = rest1.owner && '<ANONYMOUS>'; // undefined
-rest2.owner = rest2.owner && '<ANONYMOUS>'; // <ANONYMOUS>
-console.log(rest1.owner, rest2.owner);
-
-rest1.owner &&= '<ANONYMOUS>'; // undefined
-rest2.owner &&= '<ANONYMOUS>'; // <ANONYMOUS>
-console.log(rest1.owner, rest2.owner);
 //////////////////////////////////////////////////////////
 
 // // Array destructuring
@@ -354,3 +310,69 @@ console.log(rest1.owner, rest2.owner);
 // // Works with the concept of nullish values, instead of falsy values
 // // Nullish values - null, undefined
 // // Doesn't include 0, ''
+
+////////////////////////////////////////////////////////////
+
+// // Logical Assignment Operators
+
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 20,
+// };
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
+
+// // OR Assignment Operator
+
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+// console.log(rest1.numGuests, rest2.numGuests);
+// // Works beautifully except when 0 is encountered.
+// rest1.numGuests = 0;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+// console.log(rest1.numGuests, rest2.numGuests);
+
+// // Nullish Assignment Operator
+// // Solves 0 issue
+// rest1.numGuests = 0;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+// console.log(rest1.numGuests, rest2.numGuests);
+
+// // AND Assignment Operator
+// // Making restaurant owner's name anonymous
+// rest1.owner = rest1.owner && '<ANONYMOUS>'; // undefined
+// rest2.owner = rest2.owner && '<ANONYMOUS>'; // <ANONYMOUS>
+// console.log(rest1.owner, rest2.owner);
+
+// rest1.owner &&= '<ANONYMOUS>'; // undefined
+// rest2.owner &&= '<ANONYMOUS>'; // <ANONYMOUS>
+// console.log(rest1.owner, rest2.owner);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
