4f0bfd42b94113ca4a751d4cf457e8b171e3a5db
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-13 07:55:05 +0530

    coding challenge done

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -417,3 +417,39 @@ const game = {
     team2: 6.5,
   },
 };
+
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+const { team1, x: draw, team2 } = game.odds;
+// OR
+// const {odds:{team1,x:draw,team2}} = game;
+console.log(team1, draw, team2);
+
+// 6.
+game.printGoals = (...playerNames) => {
+  console.log(
+    `${playerNames.length} goals were scored and players are ${playerNames}`
+  );
+};
+
+game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+game.printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1');
+team2 < team1 && console.log('Team 2');
