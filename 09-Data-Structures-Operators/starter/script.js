02bfcd703f6435add4da431229b7b92db5e2558f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-13 15:43:35 +0530

    Coding Challenge #2 done

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -601,3 +601,69 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 
 GOOD LUCK 😀
 */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+for (const [goalNum, player] of Object.entries(game.scored)) {
+  console.log(`Goal ${+goalNum + 1}: ${player}`);
+}
+
+//2.
+let sum = 0;
+for (const odd of Object.values(game.odds)) {
+  sum += odd;
+}
+let avg = sum / Object.values(game.odds).length;
+console.log(avg.toFixed(2));
+
+//3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  console.log(`Odd of victory ${game[team] ?? 'draw'}: ${odd}`);
+}
+
+// BONUS
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
